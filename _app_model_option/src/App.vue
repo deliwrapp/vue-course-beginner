31b76d5e3ce0879a597db194345ac75faad7d0e9
DeliwrappAndCo <provider@deliwrapp.com>
2024-01-23 10:07:23 +0100

    ADD: app model option

---
+++ _app_model_option/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+/* 
+IMPORTER LES COMPOSANTS ICI 
+*/
+export default {
+  name: "App",
+  data() {
+    return {
+      // Les données à afficher dans l'application
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Monter les composants ici -->
+</template>
+
+<style scoped>
+
+
+</style>
